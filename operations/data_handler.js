9f1f7e4ca5c320623fa6761c747f6c8e4bb62996
jeremyKisner <jkisner984@gmail.com>
2021-02-07 01:28:10 -0500

    refactoring building out data retrieval

--- operations/data_handler.js
+++ operations/data_handler.js
@@ -10,10 +10,23 @@ update file entry
 
 */
 var fs = require('fs');
+var db_file = './data/data.json';
 
 var operations = {
 	add_data: function(data) {
-		console.log(data);
+		console.log('writing data');
+		fs.writeFile(db_file, data, {flag: 'w+'}, error => {
+			if (err) {
+				console.error(error);
+				return
+			}
+			console.log('received data');
+			console.log(data);
+		});
+	},
+	get_data: function() {
+		console.log('Getting data');
+		return fs.readFileSync(db_file, 'utf8');;
 	}
 }
 
--- server.js
+++ server.js
@@ -1,27 +1,9 @@
-'use strict';
-const express = require('express');
-const querystring = require('querystring');
-const data_handler = require('./operations/data_handler.js')
-const HOST = '0.0.0.0';
-const PORT = 8080;
-const app = express();
-
-app.get('/', (request, response) => {
-	response.sendFile(__dirname + "/public/" + "index.html");
+var express = require('express');
+var app = express();
+ 
+var core = require('./core');
+app.use('/api/', core);
+ 
+var server = app.listen(8000, function() {
+    console.log('Listening on port http://localhost:8000/api...');
 });
-
-app.post('/add', (request, response) => {
-	console.log('Received add request');
-	const requestBody = [];
-	
-	request.on('data', (chunks) => {
-		requestBody.push(chunks);
-	});
-
-	request.on('end', () => {
-		data_handler.data.add_data(querystring.parse(Buffer.concat(requestBody).toString()))
-	});
-});
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
